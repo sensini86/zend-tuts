dd3bc460950299303d15629273abb63f9a099b9d
Sezgin Cholak <sezgin.cholak@gmail.com>
2017-12-29 11:28:32 +0200

    tests

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -14,6 +14,21 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        var_dump(
+            nl2br(__DIR__         . " | returns the current working directory\r\n\t" . PHP_EOL),
+            nl2br(__FILE__        . " | returns the current working directory and file name" . PHP_EOL),
+            nl2br(__CLASS__       . " | returns the current working class name and namespace if definded" . PHP_EOL),
+            nl2br( __FUNCTION__    . " | returns the current function name" . PHP_EOL),
+            nl2br(__LINE__        . " | returns the current line number at the time of use point" . PHP_EOL),
+            nl2br( __NAMESPACE__   . " | returns the current namespace" . PHP_EOL),
+            nl2br(__METHOD__      . " | returns the current method name" . PHP_EOL),
+            nl2br(__TRAIT__       . " | returns the current trait name and namsepace if defined" . PHP_EOL)
+        );
+
+
+        exit;
+
         return new ViewModel();
     }
+
 }
